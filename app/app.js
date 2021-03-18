39d77d873057947663fd306e382d714af7d60520
temphJ <temphi20@gmail.com>
2021-03-19 00:10:30 +0900

    public

--- app/app.js
+++ app/app.js
@@ -5,11 +5,13 @@ const app  = express();
 const port = 2048;
 
 //router
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); //use: middle ware 등록 method
+
 
 module.exports = app;
