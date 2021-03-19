2dd5042ce1b16f764ba84fcc2a63a5fd4c6436f9
temphJ <temphi20@gmail.com>
2021-03-19 14:59:18 +0900

    dom

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+//input id: #
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginbutt = document.querySelector("button");
+
+loginbutt.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+
+    console.log(req);
+}
